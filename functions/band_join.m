0c46e811c73da8087ab2c43dfa490302f33065bd
Jose Pacheco <jap@hpe.com>
2018-11-17 10:08:08 -0600

    Fix upsampler

--- functions/band_join.m
+++ functions/band_join.m
@@ -3,13 +3,15 @@ function result = band_join(band_list, N)
         result = band_list;
     else
         low_filter = fir1(N, 1/2);
-        low_band = upsample(band_join(band_list(1:end/2,:), N), 2);
+        low_band = band_join(band_list(1:end/2,:), N);
+        low_band = upsample(low_band, 2);
         low_band = filter(low_filter, 1, low_band);
 
         high_filter = fir1(N, 1/2, 'high');
-        high_band = upsample(band_join(band_list(end/2+1:end,:), N), 2);
-        high_band = filter(high_filter, 1, high_band);
+        high_band = band_join(band_list(end/2+1:end,:), N);
         high_band(2:2:end) = high_band(2:2:end) .* -1;
+        high_band = upsample(high_band, 2);
+        high_band = filter(high_filter, 1, high_band);
 
         result = low_band + high_band;
     end
--- main_encoder.m
+++ main_encoder.m
@@ -23,7 +23,7 @@ toc;
 
 %Create sample input
 levels = 2;
-fmuestreo = 44*levels;
+fmuestreo = 88;
 tmax = 10;
 N = 60;
 
@@ -55,93 +55,5 @@ join_result = band_join(split_result, N);
 
 %Plot joined bands
 figure();
-stem(0:(fmuestreo-1), abs(fft(join_result(N*2+1:end), fmuestreo)));
+stem(0:(fmuestreo-1), abs(fft(join_result(N*2^levels+1:end), fmuestreo)));
 title(sprintf('Espectro de bandas unidas'));
-
-return;
-%
-% Debugging join_result. high_band and low_band are OK, the problem is
-% joining them together
-%
-
-figure(); %#ok<UNRCH>
-stem(0:((fmuestreo/2^levels)-1), abs(fft(split_result(1,N+1:end), (fmuestreo/2^levels))));
-title(sprintf('Espectro de banda %d', 1))
-
-
-low_filter = fir1(N, 1/2);
-low_band = upsample(split_result(1,:), 2);
-low_band = filter(low_filter, 1, low_band);
-
-figure();
-stem(0:(fmuestreo/2-1), abs(fft(low_band(N*2+1:end), fmuestreo/2)));
-title(sprintf('Espectro banda 1 upsampled'));
-
-figure();
-stem(0:((fmuestreo/2^levels)-1), abs(fft(split_result(2,N+1:end), (fmuestreo/2^levels))));
-title(sprintf('Espectro de banda %d', 2))
-
-high_filter = fir1(N, 1/2, 'high');
-high_band = upsample(split_result(2,:), 2);
-high_band = filter(high_filter, 1, high_band);
-high_band(2:2:end) = high_band(2:2:end) .* -1;
-
-figure();
-stem(0:(fmuestreo/2-1), abs(fft(high_band(N*2+1:end), fmuestreo/2)));
-title(sprintf('Espectro banda 2 upsampled'));
-
-low_low_band = high_band + low_band;
-
-figure();
-stem(0:(fmuestreo/2-1), abs(fft(low_low_band(N*2+1:end), fmuestreo/2)));
-title(sprintf('Espectro señal upsampled a %d Hz', fmuestreo/2));
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% figure();
-% stem(0:((fmuestreo/2^levels)-1), abs(fft(split_result(3,N+1:end), (fmuestreo/2^levels))));
-% title(sprintf('Espectro de banda %d', 3))
-
-
-low_filter = fir1(N, 1/2);
-low_band = upsample(split_result(3,:), 2);
-low_band = filter(low_filter, 1, low_band);
-
-% figure();
-% stem(0:(fmuestreo/2-1), abs(fft(low_band(N*2+1:end), fmuestreo/2)));
-% title(sprintf('Espectro banda 3 upsampled'));
-% 
-% figure();
-% stem(0:((fmuestreo/2^levels)-1), abs(fft(split_result(4,N+1:end), (fmuestreo/2^levels))));
-% title(sprintf('Espectro de banda %d', 4))
-
-high_filter = fir1(N, 1/2, 'high');
-high_band = upsample(split_result(4,:), 2);
-high_band = filter(high_filter, 1, high_band);
-high_band(2:2:end) = high_band(2:2:end) .* -1;
-
-figure();
-stem(0:(fmuestreo/2-1), abs(fft(high_band(N*2+1:end), fmuestreo/2)));
-title(sprintf('Espectro banda 4 upsampled'));
-
-high_high_band = high_band + low_band;
-
-figure();
-stem(0:(fmuestreo/2-1), abs(fft(high_high_band(N*2+1:end), fmuestreo/2)));
-title(sprintf('Espectro señal upsampled a %d Hz', fmuestreo/2));
-
-%%%%
-low_filter = fir1(N, 1/2);
-low_band = upsample(low_low_band, 2);
-low_band = filter(low_filter, 1, low_band);
-
-high_filter = fir1(N, 1/2, 'high');
-high_band = upsample(high_high_band, 2);
-high_band = filter(high_filter, 1, high_band);
-high_band(2:2:end) = high_band(2:2:end) .* -1;
-
-result = low_band + high_band;
-
-figure();
-stem(0:(fmuestreo-1), abs(fft(result(N*2+1:end), fmuestreo)));
-title(sprintf('Espectro señal upsampled a %d Hz', fmuestreo));
