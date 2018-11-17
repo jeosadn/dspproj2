43d500ed87ec8e1522caa1464a3036da6f5d44d5
Jose Pacheco <jap@hpe.com>
2018-11-17 12:27:34 -0600

    Tried band splitting in enconder.m, it works ok.

--- functions/encoder.m
+++ functions/encoder.m
@@ -6,10 +6,16 @@ function encoder(audio_input_filename, audio_output_filename)
     % 16 bit per sample, at 44100 Hz, in stereo
     [sampleData, sampleFrequency] = audioread(audio_input_filename);
 
+    %Split in bands
+    split_result = band_split(sampleData.', 60, 2);
+
+    %Rejoin bands
+    join_result = band_join(split_result, 60);
+
     %--------------------------------------------------------------------------
     % Write audio file
     %--------------------------------------------------------------------------
     %Output characteristics:
     % 16 bit per sample, at 44100 Hz, in stereo
-    audiowrite(audio_output_filename, sampleData, sampleFrequency);
+    audiowrite(audio_output_filename, join_result, sampleFrequency);
 end
