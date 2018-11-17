08cb588dc421293e0ae6c7eb56c35bb76f0a0c04
Jose Pacheco <jap@hpe.com>
2018-11-16 20:22:35 -0600

    Added real band splitting
    For some reason, scaling after decimating doesn't seem to be necessary

--- functions/band_split.m
+++ functions/band_split.m
@@ -1,21 +1,16 @@
-function result = band_split(original_signal, levels)
-    %Data length check
-    while mod(log2(length(original_signal)), 1) ~= 0
-        original_signal = [original_signal 0]; %#ok<AGROW>
-    end
-
-    %Band number check
-    if length(original_signal) < 2^levels
-        fprintf('Error: Not enough data for that amount of levels\n');
-        result = [];
-        return
-    end
-
-    %Recursive processing
+function result = band_split(original_signal, N, levels)
     if levels == 0;
         result = original_signal;
     else
-        low_band = band_split(original_signal(1:end/2), levels-1);
-        high_band = band_split(original_signal(end/2+1:end), levels-1);
+        low_filter = fir1(N, 1/2);
+        low_band = filter(low_filter, 1, original_signal);
+        low_band = low_band(1:2:end);
+        low_band = band_split(low_band, N, levels-1);
+
+        high_filter = fir1(N, 1/2, 'high');
+        high_band = filter(high_filter, 1, original_signal);
+        high_band = high_band(1:2:end);
+        high_band(2:2:end) = high_band(2:2:end) .* -1;
+        high_band = band_split(high_band, N, levels-1);
         result = [low_band; high_band];
     end
--- main_encoder.m
+++ main_encoder.m
@@ -17,7 +17,36 @@ encoder(char(audio_input_filename), char(audio_output_filename));
 fprintf('Coding time:\n');
 toc;
 
+%
 %Band splitter
-original_signal = [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16]
-split_result = band_split(original_signal, 2)
-join_result = band_join(split_result)
+%
+
+%Create sample input
+fmuestreo = 88;
+tmax = 10;
+N = 60;
+div = 2;
+
+fsenal = [1 2 3  14 15 16  25 26 27  36 37 38];
+asenal = [1 3 5   7 11  9  17 15 13  21 19 23];
+
+tvecmues = 0:1/fmuestreo:tmax-(1/fmuestreo);
+smues = zeros(1, length(tvecmues));
+for i = 1:length(fsenal)
+    smues = smues + asenal(i)*cos(2*pi*fsenal(i).*tvecmues);
+end
+
+%Plot sample input
+figure();
+stem(0:(fmuestreo-1), abs(fft(smues, fmuestreo)));
+title(sprintf('Espectro señal muestreada a %d Hz', fmuestreo));
+
+%Split in bands
+split_result = band_split(smues, N, 2);
+
+%Plot split bands
+for i = 1:size(split_result, 1)
+    figure();
+    stem(0:(fmuestreo-1), abs(fft(split_result(i,N+1:end), fmuestreo)));
+    title(sprintf('Espectro de banda %d', i))
+end
